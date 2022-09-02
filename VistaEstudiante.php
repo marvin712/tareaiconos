adcf2ad0ba0728f33984bc2a69e7b7ec55823c1d
marvin712 <62572934+marvin712@users.noreply.github.com>
2022-09-01 19:43:56 -0600

    Primer Commit

---
+++ VistaEstudiante.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        img{
+            width: 20px;
+            height: 20px;
+        }
+        th,
+        td{
+            text-align:center;
+        }
+    </style>
+</head>
+<body>
+    <p>
+        <h2>INGRESO DE DATOS DEL ESTUDIANTE</h2>
+    </p>
+    <form action="EstudianteController.php" method="post">
+        <p>
+            <label for="apellidos">Apellidos</label>
+            <input type="text" name="apellidos">
+        </p>
+        
+        <p>
+            <label for="nombre">Nombre</label>
+        <input type="text" name="nombre">
+        </p>
+        
+        <p>
+            <label for="direccion">direccion</label>
+            <input type="text" name="direccion">
+        </p>
+        
+        <p>
+            <label for="telefono">Telefono</label>
+            <input type="number" name="telefono">
+        </p>
+        
+        <p>
+            <input type="submit" value="Guardar Registro">
+        </p>
+    </form>
+    <hr>
+    <h3>LISTADO DE ESTUDIANTES</h3>
+    <hr>
+    <table border="1">
+        <th>NOMBRE</th>
+        <th>APELLIDOS</th>
+        <th>DIRECCION</th>
+        <th>TELEFONO</th>
+        <th colspan="2">OPERACIONES</th>
+    <?php
+        include_once "EstudianteModel.php";
+        $Estudiante = new Estudiante();
+        $ListaEstudiantes = $Estudiante->ListarEstudiantes();
+        while($Estudiantes = mysqli_fetch_assoc($ListaEstudiantes))
+        {?>
+          <tr>
+                <td>  <?php echo $Estudiantes['nombre'] ?> </td>
+                <td>  <?php echo $Estudiantes['apellidos'] ?> </td>
+                <td>  <?php echo $Estudiantes['direccion'] ?> </td>
+                <td>  <?php echo $Estudiantes['telefono'] ?> </td>
+                <td><a href="editar.html"><img src="editar.svg" alt=""></a></td>
+                <td><a href="eliminar.html"><img src="eliminar.svg" alt=""></a></td>
+          </tr>
+          
+       <?php } ?>
+    
+    </table>
+</body>
+</html>
